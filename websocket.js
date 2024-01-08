0ea53e5e7a3f27c8db7ac283c56ebe816ebecd38
Emre Aykin <emre.kou.1809@gmail.com>
2024-01-08 19:49:14 +0300

    oto restart test

--- websocket.js
+++ websocket.js
@@ -6,7 +6,7 @@ export const startWebsocket = async (server) => {
     wss.on("connection", async (ws) => {
       console.log("Kullanıcı bağlandı..");
       setInterval(() => {
-        ws.send(JSON.stringify({ label: "Merhaba deneme oto..." }));
+        ws.send(JSON.stringify({ label: "Merhaba deneme oto... oto restart deneme" }));
       }, 2000);
       ws.on("message", async (message) => {
         let messageStr = message.toString();
