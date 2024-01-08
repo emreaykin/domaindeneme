ea03ed05c53ad72c2f44c6610b7a5a7c773adcac
Emre Aykin <emre.kou.1809@gmail.com>
2024-01-08 19:58:06 +0300

    test6

--- websocket.js
+++ websocket.js
@@ -6,7 +6,7 @@ export const startWebsocket = async (server) => {
     wss.on("connection", async (ws) => {
       console.log("Kullanıcı bağlandı..");
       setInterval(() => {
-        ws.send(JSON.stringify({ label: "Merhaba deneme oto testw244" }));
+        ws.send(JSON.stringify({ label: "Merhaba deneme oto tesatw244" }));
       }, 2000);
       ws.on("message", async (message) => {
         let messageStr = message.toString();
