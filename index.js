908928b8fdf2de39a90322637e0b383dbe9fb7d4
Emre Aykin <emre.kou.1809@gmail.com>
2024-02-02 13:52:13 +0300

    port deneme

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { startWebsocket } from "./websocket.js";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(express.json());
 app.use(cors());
@@ -21,8 +21,8 @@ app.get('/api/hello', (req, res) => {
 app.post('/api/goodbye', (req, res) => {
   res.json({ message: 'Güle güle!' });
 });
-const server = app.listen(PORT, async () => {
-  console.log(`Uygulama http://localhost:${PORT} çalışıyor `);
+const server = app.listen( async () => {
+  console.log(`Uygulama http://localhost: çalışıyor `);
   
   startWebsocket(server);
 });
